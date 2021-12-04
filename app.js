a8df587c899d35df4f53c7aa480f445f1207c291
Wesley Ford <mangofordw@gmail.com>
2021-12-04 01:31:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.command('/issue', async ({ command, ack, respond }) => {
    let repoIssue = await octokit.rest.issues.listForRepo({
       owner: 'sergix',
       repo: command.text,
+   }).then(data => {
+      data.json()
+
+      return data.length
    });
    await respond(repoIssue);
 });
