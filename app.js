3191c33b7e346b1086c748c6df0a74378a1a2da5
Wesley Ford <mangofordw@gmail.com>
2021-12-04 17:28:08 -0600

    Fixed the issue command
    The Issue command now responds back into the chat for others to see

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const app = new App({
 
 
 // The echo command simply echoes on command
-app.command('/issue', async ({ command, ack, respond }) => {
+app.command('/issue', async ({ command, ack, say }) => {
    // Acknowledge command request
    await ack().then(() => {
       return commands.splitArgs(command.text)
    }).then((data) => {
       return github.getReposIssues(data[0], data[1])
    }).then((block) => {
-      respond(block)
+      say(block)
    });
 });
 
