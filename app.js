39d29a17202b341c54059be39ad08fdcd28c406c
Wesley Ford <mangofordw@gmail.com>
2021-12-03 23:59:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,29 +20,8 @@ const app = new App({
    console.log(`⚡️ Slack Bolt app is running!`);
 })();
 
-// Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-   // say() sends a message to the channel where the event was triggered
-   await say({
-     blocks: [
-       {
-         "type": "section",
-         "text": {
-           "type": "mrkdwn",
-           "text": `Hey there <@${message.user}>!`
-         },
-         "accessory": {
-           "type": "button",
-           "text": {
-             "type": "plain_text",
-             "text": "Click Me"
-           },
-           "action_id": "button_click"
-         }
-       }
-     ],
-     text: `Hey there <@${message.user}>!`
-   });
+   await say(`Hello, <@${message.user}>`);
  });
 
 app.error((error) => {
