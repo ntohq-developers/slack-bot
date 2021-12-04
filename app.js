26b7657bb19dae57b6638a0ab8c04a211eb47474
Wesley Ford <mangofordw@gmail.com>
2021-12-03 23:13:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -32,10 +32,36 @@ app.event("member_joined_channel", async ({ event, client }) => {
       });
       console.log(result);
    }
+
    catch (error) {
       console.error(error);
    }
 })
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+   // say() sends a message to the channel where the event was triggered
+   await say({
+     blocks: [
+       {
+         "type": "section",
+         "text": {
+           "type": "mrkdwn",
+           "text": `Hey there <@${message.user}>!`
+         },
+         "accessory": {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Click Me"
+           },
+           "action_id": "button_click"
+         }
+       }
+     ],
+     text: `Hey there <@${message.user}>!`
+   });
+ });
 
 app.error((error) => {
    // Check the details of the error to handle cases where you should retry sending a message or stop the app
