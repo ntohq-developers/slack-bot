708fb2ae25310b636eac01e429efe0ec6c64030e
Wesley Ford <mangofordw@gmail.com>
2021-12-04 20:40:36 -0600

    Add error catching for the /issue command

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { App }           = require("@slack/bolt")
 const github         = require("./github")
 const commands = require("./commands.js")
 
+const errorBlock = require('./block_templates/error/message.json')
+
 // dotenv setup
 require("dotenv").config()
 
@@ -16,15 +18,22 @@ const app = new App({
 
 
 // The echo command simply echoes on command
-app.command('/issue', async ({ command, ack, say }) => {
-   // Acknowledge command request
-   await ack().then(() => {
-      return commands.splitArgs(command.text)
-   }).then((data) => {
-      return github.getReposIssues(data[0], data[1])
-   }).then((block) => {
-      say(block)
-   });
+app.command('/issue', async ({ command, ack, say, respond }) => {
+   try
+   {
+      await ack().then(() => {
+         return commands.splitArgs(command.text)
+      }).then((data) => {
+         return github.getReposIssues(data[0], data[1])
+      }).then((block) => {
+         say(block)
+      });
+   }
+   catch(err)
+   {
+      console.log(err)
+      respond(errorBlock)
+   }
 });
 
 (async () => {
